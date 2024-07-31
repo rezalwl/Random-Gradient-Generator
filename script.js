f7a0b4b17f795027bb5d9c14350773c015c48e67
Mohamadreza Azizi <154751813+rezalwl@users.noreply.github.com>
2024-07-31 18:06:56 +0330

    update

--- script.js
+++ script.js
@@ -1,16 +1,56 @@
+const $ = document;
+const firstColorInput = $.querySelector("#color-a");
+const secondColorInput = $.querySelector("#color-b");
+const directions = $.querySelectorAll(".buttons button");
+const generateBtn = $.querySelector("#submit");
+const codeTextArea = $.querySelector("#code");
+const copyBtn = $.querySelector("#copy");
+const copyTooltip = $.querySelector("#myTooltip");
 
-document.getElementById("btn").addEventListener("click", function () { 
-let a = Math.floor(Math.random() * 255);
-let b = Math.floor(Math.random() * 255);
-let c = Math.floor(Math.random() * 255);
-let d = Math.floor(Math.random() * 255);
-let e = Math.floor(Math.random() * 255);
-let f = Math.floor(Math.random() * 255);
-let g = (Math.random().toFixed(1));
-let h = (Math.random().toFixed(1));
+let currentDirection = "to bottom";
 
-document.body.style.backgroundImage = `linear-gradient(to right, rgba(${a},${b},${c},${g}), rgba(${c},${d},${e},${h}))`;
-  console.log(a , g)
-  document.querySelector(".color").textContent = `linear-gradient(to right, rgba(${a},${b},${c},${g}), rgba(${c},${d},${e},${h}))`;
+const setDirection = (direction, directionElem) => {
+  for (let item of directions) {
+    item.classList.remove("active");
+  }
+  if (directionElem.tagName === "BUTTON") {
+    directionElem.classList.add("active");
+  } else {
+    directionElem.parentElement.classList.add("active");
+  }
+  currentDirection = direction;
+};
+
+const generateCssCode = () => {
+  let cssCode = `background: linear-gradient(${currentDirection}, ${firstColorInput.value}, ${secondColorInput.value})`;
+
+  codeTextArea.value = cssCode;
+
+  document.body.style.cssText = cssCode;
+};
+
+const copyCssCode = () => {
+  codeTextArea.select();
+  navigator.clipboard.writeText(codeTextArea.value);
+  copyTooltip.innerHTML = "Copy successful";
+  copyTooltip.style.backgroundColor = "green";
+};
+
+const outFunc = () => {
+  copyTooltip.innerHTML = "Copy to clipboard";
+  copyTooltip.style.backgroundColor = "#555";
+};
+
+directions.forEach((direction) => {
+  direction.addEventListener("click", (event) => {
+    let direction = event.target.dataset.direction;
+
+    setDirection(direction, event.target);
+  });
 });
 
+generateBtn.addEventListener("click", generateCssCode);
+copyBtn.addEventListener("click", copyCssCode);
+copyBtn.addEventListener("mouseout", outFunc);
+
+generateCssCode();
